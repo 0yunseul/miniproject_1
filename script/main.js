07d367cb1ebcb8bef4eff0d193885293b21a4a7b
jooyoung <jooyoung.cairo@gmail.com>
2022-10-06 15:04:11 +0900

    second commit

---
+++ script/main.js
@@ -0,0 +1,10 @@
+// .top 인 요소를 기준으로 다음요소를 찾아서 있으면 (현재요소) .top을 제거하고 다음요소에 .top을 추가하는데, 3초에 한번씩 순환
+// $()
+// .querySelector()
+const sceneIntervalID = setInterval(changeScene, 3000);
+function changeScene(){
+    // 현재요소 찾고
+    // 다음요소 찾아서
+    // 있으면 .top을 전달(현재에서 제거, 다음에 붙이기)
+    // 없으면 강제로 1번에 다시 전달하고 3초뒤에 원래 흐름대로 반복/순환
+}
