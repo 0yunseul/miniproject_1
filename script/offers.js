74ab12fccce7b40e675d7abcde89c34aebf1fdbe
jooyoung <jooyoung.cairo@gmail.com>
2022-10-08 20:50:18 +0900

    edited

--- script/offers.js
+++ script/offers.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function(){
-    setTimeout(showAfterLoad, 3000);
-    const POSTS = document.querySelector("#posts");
+    setTimeout(showAfterLoad, 1000);
+  
     const POPUP = document.querySelector("#popup");
     const BODY  = document.querySelector("body");
     const CLOSE = document.querySelector("#close");
@@ -13,24 +13,9 @@ window.addEventListener("DOMContentLoaded", function(){
     CLOSE.addEventListener("click", function(){
         POPUP.style.display = "none";
         BODY.style.overflow = "auto";	
+
+
     })
-    const NEXT = document.querySelector(".next");
-    const PREV = document.querySelector(".prev");
-    let postIndex = 0;		
-        
-    NEXT.addEventListener("click",function(){
-        postIndex++;
-        if(postIndex>=3) postIndex = 0;
-        let coords = -920 * postIndex;
-        POSTS.style.marginLeft = coords+"px";
-    })
-    PREV.addEventListener("click",function(){
-        postIndex--;
-        if(postIndex<0 ) postIndex = 2;
-        let coords = -920 * postIndex;
-        POSTS.style.marginLeft = coords+"px";
-    })
+ })
     
-    
-    
-})
+
